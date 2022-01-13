6132b4b997f21f7152a334bb554108877c318617
ALMA_DEV914 <aungonalna58@gamil.com>
2022-01-13 14:27:59 -0800

    add comment model and routes

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-
+const dateFormat = require('../utils/dateFormat');
 const PizzaSchema = new Schema({
     pizzaName: {
         type: String
@@ -7,17 +7,36 @@ const PizzaSchema = new Schema({
     createdBy: {
         type: String
     },
-    craetedAt: {
+    createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
       type: String,
       default: 'Large'
     },
-    toppings: []
+    toppings: [],
+    comments: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Comment'
+        }
+      ]
+    },
+    {
+        toJSON: {
+          virtuals: true,
+          getters: true
+        },
+        id: false
+      }
+);
 
-});
+    // get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
+  });
 
 //create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
